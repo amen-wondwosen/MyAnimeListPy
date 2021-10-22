8d99baa04bd6d6bff0dd8c401535c0306d438493
Amen Wondwosen <amenwond408@gmail.com>
2021-10-22 15:52:57 -0700

    Matched mal only data format with combined data format
    Added a function to reformat data only scraped from MyAnimeList.net to a common format.

--- utils/__init__.py
+++ utils/__init__.py
@@ -21,7 +21,7 @@ def combine_sources(mal, al):
 
     if "errors" in al.keys():
         # If no metadata from anilist
-        return mal
+        return mal_all(mal)
 
     # For simplicity
     al = al["data"]["Media"]
@@ -80,6 +80,61 @@ def combine_sources(mal, al):
 
     for edge in al["studios"]["edges"]:
         j_data["studios"].append(edge["node"]["name"])
+
+    j_data["rating"] = mal["rating"]
+
+    return j_data
+
+
+def mal_all(mal):
+    '''
+    Reformats the mal data into the same format as
+    shown in the combine_sources function
+    '''
+    j_data = {
+        "idMal": "", "idAL": "", "title": "",
+        "english": [], "japanese": [], "romaji": [], "synonyms": [], "native": [],
+        "type": "",
+        "episodes": 0, "status": "", "aired": "", "season": "", "year": "",
+        "demographic": "", "genres": [], "tags": [], "theme": "",
+        "source": "",
+        "licensors": [], "studios": [],
+        "rating": ""
+    }
+
+    j_data["idMal"] = mal["id"]
+
+    j_data["title"] = mal["title"]
+
+    j_data["english"] += mal.get("english", [])
+
+    j_data["japanese"] += mal.get("japanese", [])
+
+    j_data["synonyms"] += mal.get("synonyms", [])
+
+    j_data["type"] = mal["type"]
+
+    j_data["episodes"] = mal["episodes"]
+
+    j_data["status"] = mal["status"]
+
+    if "premiered" in mal.keys():
+        j_data["season"], j_data["year"] = mal["premiered"].split()
+
+    j_data["demographic"] = mal.get("demographic", "")
+
+    j_data["genres"] = mal["genres"]
+    j_data["theme"] = mal.get("theme", "")
+    
+    j_data["source"] = mal["source"]
+
+    if mal["licensors"] != "None found":
+        if type(mal["licensors"]) == str:
+            j_data["licensors"].append(mal["licensors"])
+        else:
+            j_data["licensors"] = mal["licensors"]
+
+    j_data["studios"] = mal.get("studios", [])
 
     j_data["rating"] = mal["rating"]
 
