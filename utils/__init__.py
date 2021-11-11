9782759fff3be8e2731ae274dd0e8aa0eec55d11
Amen Wondwosen <amenwond408@gmail.com>
2021-11-10 19:37:51 -0800

    Update __init__.py
    It's ugly. Try to make it look better.

--- utils/__init__.py
+++ utils/__init__.py
@@ -28,17 +28,17 @@ def combine_sources(mal, al):
     al = al["data"]["Media"]
 
     j_data = {
-        "idMal": "", "idAL": "", "title": "",
+        "idMal": "", "idAL": None, "title": "",
         "english": [], "native": [],
         "type": "",
         "episodes": 0, "status": "",
-        "airing_date": {
-            "start": "????-??-??",
-            "end": "????-??-??"
+        "aired": {
+            "start": None,
+            "end": None
         },
         "season": {
-            "season": "",
-            "year": ""
+            "season": None,
+            "year": None
         },
         "category": {
             "demographic": "",
@@ -74,10 +74,10 @@ def combine_sources(mal, al):
     j_data["status"] = mal["status"]
 
     airing_date = _parse_airing_date(mal["aired"])
-    j_data["airing_date"]["start"], j_data["airing_date"]["end"] = airing_date
+    j_data["aired"]["start"], j_data["aired"]["end"] = airing_date
 
-    j_data["season"]["season"] = al["season"]
-    j_data["season"]["year"] = al["seasonYear"]
+    if al["season"] != None: j_data["season"]["season"] = al["season"]
+    if al["seasonYear"] != None: j_data["season"]["year"] = al["seasonYear"]
 
     j_data["category"]["demographic"] = mal.get("demographic", "")
     j_data["category"]["theme"] = mal.get("theme", "")
@@ -87,10 +87,12 @@ def combine_sources(mal, al):
     j_data["source"] = mal["source"]
 
     if mal["licensors"] != "None found":
-        if type(mal["licensors"]) == str:
+        if (type(mal["licensors"]) == str) and (mal["licensors"] != ""):
             j_data["licensors"].append(mal["licensors"])
         else:
-            j_data["licensors"] = mal["licensors"]
+            for licensor in mal["licensors"]:
+                if licensor != "":
+                    j_data["licensors"].append(licensor)
 
     for edge in al["studios"]["edges"]:
         j_data["studios"].append(edge["node"]["name"])
@@ -106,17 +108,17 @@ def mal_all(mal):
     shown in the combine_sources function
     '''
     j_data = {
-        "idMal": "", "idAL": "", "title": "",
+        "idMal": "", "idAL": None, "title": "",
         "english": [], "native": [],
         "type": "",
         "episodes": 0, "status": "",
-        "airing_date": {
-            "start": "????-??-??",
-            "end": "????-??-??"
+        "aired": {
+            "start": None,
+            "end": None
         },
         "season": {
-            "season": "",
-            "year": ""
+            "season": None,
+            "year": None
         },
         "category": {
             "demographic": "",
@@ -137,7 +139,7 @@ def mal_all(mal):
     j_data["english"] += list(set(mal.get("english", []) \
                         + mal.get("synonyms", [])))
 
-    j_data["japanese"] += mal.get("japanese", [])
+    j_data["native"] += mal.get("japanese", [])
 
     j_data["type"] = mal["type"]
 
@@ -146,7 +148,7 @@ def mal_all(mal):
     j_data["status"] = mal["status"]
 
     airing_date = _parse_airing_date(mal["aired"])
-    j_data["airing_date"]["start"], j_data["airing_date"]["start"] = airing_date
+    j_data["aired"]["start"], j_data["aired"]["end"] = airing_date
 
     if "premiered" in mal.keys():
         j_data["season"], j_data["year"] = mal["premiered"].split()
@@ -156,10 +158,12 @@ def mal_all(mal):
     j_data["category"]["genres"] = list(set(mal["genres"]))
 
     if mal["licensors"] != "None found":
-        if type(mal["licensors"]) == str:
+        if (type(mal["licensors"]) == str) and (mal["licensors"] != ""):
             j_data["licensors"].append(mal["licensors"])
         else:
-            j_data["licensors"] = mal["licensors"]
+            for licensor in mal["licensors"]:
+                if licensor != "":
+                    j_data["licensors"].append(licensor)
 
     j_data["studios"] = mal.get("studios", [])
 
@@ -170,23 +174,63 @@ def mal_all(mal):
 
 def _parse_airing_date(airing_date):
 
-    start_date = end_date = "????-??-??"
+    start_date = end_date = None
+
+    if airing_date == "Not available": return (start_date, end_date)
 
     if " to " not in airing_date:
-        # only one date
-        start_date = end_date = dt_obj = str(datetime.strptime(airing_date, "%b %d, %Y").date())
+        try:
+            start_date = end_date = dt_obj = str(datetime.strptime(airing_date, "%b %d, %Y").date())
+        except: pass
+
+        try:
+            start_date = end_date = dt_obj = str(datetime.strptime(airing_date, "%b %Y").date())
+        except: pass
+
+        try:
+            start_date = end_date = dt_obj = str(datetime.strptime(airing_date, "%Y").date())
+        except:
+            pass
+    
     else:
-        # safe to assume only two dates
         start_date, end_date = airing_date.split(" to ")
 
         if "?" in start_date:
-            start_date = "????-??-??"
+            start_date = None
         else:
-            start_date = str(datetime.strptime(start_date, "%b %d, %Y").date())
+            try:
+                start_date = dt_obj = str(datetime.strptime(start_date, "%b %d, %Y").date())
+            except: pass
+
+            try:
+                start_date = dt_obj = str(datetime.strptime(start_date, "%b %Y").date())
+            except: pass
+
+            try:
+                start_date = dt_obj = str(datetime.strptime(start_date, "%Y").date())
+            except:
+                pass
 
         if "?" in end_date:
-            end_date = "????-??-??"
+            end_date = None
         else:
-            end_date = str(datetime.strptime(end_date, "%b %d, %Y").date())
+            try:
+                end_date = dt_obj = str(datetime.strptime(end_date, "%b %d, %Y").date())
+            except: pass
+
+            try:
+                end_date = dt_obj = str(datetime.strptime(end_date, "%b %Y").date())
+            except: pass
+
+            try:
+                end_date = dt_obj = str(datetime.strptime(end_date, "%Y").date())
+            except:
+                pass
+
+            end_date = max(start_date, end_date)
 
     return (start_date, end_date)
+
+def _date_to_season(d):
+    '''Converts the date to the corresponding season and year'''
+    pass
